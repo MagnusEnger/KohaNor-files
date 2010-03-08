84520717cabd1f1cbf05fdc89dd2756914a9a9a0
Magnus Enger <magnus@enger.priv.no>
2010-03-08 12:11:27 +0100

    Split libraries into public and research

--- categories.sql
+++ categories.sql
@@ -1,7 +1,8 @@
 INSERT INTO `categories` VALUES 
 ('ANS','Ansatt',120,0,0,NULL,NULL,'0.000000',0,NULL,'0.000000','S'),
 ('BIB','Bibliotekar',120,0,0,NULL,NULL,'0.000000',0,NULL,'0.000000','S'),
-('BIBLIOTEK','Folke- og fagbibliotek',120,0,0,NULL,NULL,'0.000000',0,NULL,'0.000000','I'),
+('FOLK','Folkebibliotek',120,0,0,NULL,NULL,'0.000000',0,NULL,'0.000000','I'),
+('FAGB','Fagbibliotek',120,0,0,NULL,NULL,'0.000000',0,NULL,'0.000000','I'),
 ('VOK','Voksen',36,18,0,NULL,NULL,'0.000000',0,NULL,'0.000000','A'),
 ('UNG','Ungdom',36,13,18,NULL,NULL,'0.000000',0,NULL,'0.000000','A'),
 ('ELE','Elev',36,0,0,NULL,NULL,'0.000000',0,NULL,'0.000000','A'),
